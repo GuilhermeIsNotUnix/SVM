3144c04171bb2fba7b70824810e94929ad68d498
Guilherme Almino Carvalho <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2018-10-14 14:26:25 -0300

    Update

---
+++ main.js
@@ -0,0 +1,55 @@
+var entrada = "";
+
+function botaoUm() {
+    entrada = entrada + 1;
+}
+
+function botaoDois() {
+    entrada = entrada + 2;
+}
+
+function botaoTres() {
+    entrada = entrada + 3;
+}
+
+function botaoQuatro() {
+    entrada = entrada + 4;
+}
+
+function botaoCinco() {
+    entrada = entrada + 5;
+}
+
+function botaoSeis() {
+    entrada = entrada + 6;
+}
+
+function botaoSete() {
+    entrada = entrada + 7;
+}
+
+function botaoOito() {
+    entrada = entrada + 8;
+}
+
+function botaoNove() {
+    entrada = entrada + 9;
+}
+
+function botaoZero() {
+    entrada = entrada + 0;
+}
+
+//Revisar (Nao esta pronto)
+function branco() {
+    entrada = "";
+}
+
+function corrige() {
+    entrada = "";
+}
+
+function confirma() {
+    alert(entrada);
+    entrada = "";
+}
