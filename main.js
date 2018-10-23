f8b802937fc5c1175ed396a37ab35df100ed768a
Guilherme Almino Carvalho <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2018-10-23 19:21:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,57 @@
 var entrada = "";
 
-function botaoUm() {
-    entrada = entrada + 1;
-}
-
-function botaoDois() {
-    entrada = entrada + 2;
-}
-
-function botaoTres() {
-    entrada = entrada + 3;
-}
-
-function botaoQuatro() {
-    entrada = entrada + 4;
-}
-
-function botaoCinco() {
-    entrada = entrada + 5;
-}
-
-function botaoSeis() {
-    entrada = entrada + 6;
-}
-
-function botaoSete() {
-    entrada = entrada + 7;
-}
-
-function botaoOito() {
-    entrada = entrada + 8;
-}
-
-function botaoNove() {
-    entrada = entrada + 9;
-}
-
-function botaoZero() {
-    entrada = entrada + 0;
-}
-
-//Revisar (Nao esta pronto)
-function branco() {
-    entrada = "";
-}
-
-function corrige() {
-    entrada = "";
-}
-
-function confirma() {
-    alert(entrada);
-    entrada = "";
+window.onload = function() {
+    document.getElementById("botao0").addEventListener("click", function() {
+        entrada = entrada + 0;
+    });
+    
+    document.getElementById("botao1").addEventListener("click", function() {
+        entrada = entrada + 1;
+    });
+    
+    document.getElementById("botao2").addEventListener("click", function() {
+        entrada = entrada + 2;
+    });
+    
+    document.getElementById("botao3").addEventListener("click", function() {
+        entrada = entrada + 3;
+    });
+    
+    document.getElementById("botao4").addEventListener("click", function() {
+        entrada = entrada + 4;
+    });
+    
+    document.getElementById("botao5").addEventListener("click", function() {
+        entrada = entrada + 5;
+    });
+    
+    document.getElementById("botao6").addEventListener("click", function() {
+        entrada = entrada + 6;
+    });
+    
+    document.getElementById("botao7").addEventListener("click", function() {
+        entrada = entrada + 7;
+    });
+    
+    document.getElementById("botao8").addEventListener("click", function() {
+        entrada = entrada + 8;
+    });
+    
+    document.getElementById("botao9").addEventListener("click", function() {
+        entrada = entrada + 9;
+    });
+    
+    document.getElementById("botaoBranco").addEventListener("click", function() {
+        entrada = "";
+        alert("Voto em branco");
+    });
+    
+    document.getElementById("botaoCorrige").addEventListener("click", function() {
+        alert("Corrigido. Digite de novo seu voto");
+    });
+    
+    document.getElementById("botaoConfirma").addEventListener("click", function() {
+        alert(entrada);
+        entrada = "";
+    });   
 }
